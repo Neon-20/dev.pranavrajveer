2637b085885b18148609b30e711effb958c40363
Pranav Rajveer <rajveer23pr@gmail.com>
2023-11-13 21:30:54 +0530

    text-change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,8 +45,8 @@ export default function Home() {
           id="subtitle"
           className="rounded-full bg-white bg-opacity-80 font-normal max-w-max mx-auto mt-8 text-sm md:text-xl p-[1px] md:p-[1.5px]"
         >
-          <div className="px-3 py-1 rounded-full bg-black bg-opacity-60 duration-300 cursor-pointer hover:bg-black text-white">
-            Hang out with me here.
+          <div className="px-4 py-2 rounded-full bg-black bg-opacity-60 duration-300 cursor-pointer hover:bg-black text-white">
+            Connect with me here 🍻
           </div>
         </div>
         </Link>
